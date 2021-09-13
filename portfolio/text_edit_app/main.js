a462a40747348c05a51c1adf5cb86fe7aac13296
hisashi2002 <hisashi2002@gmail.com>
2021-09-14 00:20:02 +0900

    edit_text_app

--- portfolio/text_edit_app/main.js
+++ portfolio/text_edit_app/main.js
@@ -1,14 +1,13 @@
-
+const ptx = document.querySelector('#paste_text');
 const edit_btn = document.querySelector('#btn');
-console.log(edit_btn)
 
 edit_btn.addEventListener('click', function() {
-    const ptx = document.querySelector('#paste_text');
-    console.log(ptx);
-    const innerPtx = ptx.textContent;
-    console.log(innerPtx)
-    let blob = new Blob([innerPtx],{type:"text/plain"});
-    let link = document.createElement('a'); 
-    link.href = URL.createObjectURL(blob);
-    link.download = 'text_edit_file';
+  let innerPtx = ptx.value;
+  let blob = new Blob([innerPtx],{type:"text/plain"});
+  let link = document.createElement('a');
+  link.href = URL.createObjectURL(blob);
+  link.download = 'text_edit_file';
+  document.body.appendChild(link);
+  link.click();
+  link.parentNode.removeChild(link)
 });
